0e7f77d10f6c9340f3f73093cf2041c010394dcf
Vinicius Branco <viniblima2016@gmail.com>
2023-03-23 19:00:08 -0300

    feat: :sparkles: adiciona configuracao de cors

--- cmd/routes.go
+++ cmd/routes.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/viniblima/go_pq/routes"
 )
 
 func setupRoutes(app *fiber.App) {
+	app.Use(cors.New())
 
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	api := app.Group("/api")
 
 	setupV1Routes(api)
